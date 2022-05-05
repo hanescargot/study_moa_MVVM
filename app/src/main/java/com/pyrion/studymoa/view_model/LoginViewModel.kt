85dde345bc2fe8d54b57291cd6bb84567c1dfac6
xian <dpyrion@gmail.com>
2022-05-05 21:57:15 +0900

    wiP

--- app/src/main/java/com/pyrion/studymoa/view_model/LoginViewModel.kt
+++ app/src/main/java/com/pyrion/studymoa/view_model/LoginViewModel.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
+    private val _tesxt = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
